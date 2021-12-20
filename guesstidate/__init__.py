dcb18eccbf1461ed80a5ba13753248621187a927
Alessandro Berti <a.berti@pads.rwth-aachen.de>
2021-12-20 14:52:35 +0100

    updated information on author

--- guesstidate/__init__.py
+++ guesstidate/__init__.py
@@ -1,4 +1,4 @@
-__author__ = 'jeffrey.starr@ztoztechnologies.com'
-__version__ = "0.4.1.3"
+__author__ = 'jeffrey.starr@ztoztechnologies.com, sebastiaan.van.zelst@fit.fraunhofer.de, alessandro.berti@fit.fraunhofer.de'
+__version__ = "0.4.1.4"
 
 from guesstidate.infer import infer
