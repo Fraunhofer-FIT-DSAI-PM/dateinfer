8f1b6fdfb5cf007b37c7bdfec5eb918ab1748874
Alessandro Berti <a.berti@pads.rwth-aachen.de>
2021-12-20 14:46:14 +0100

    changed details

--- setup.py
+++ setup.py
@@ -15,7 +15,7 @@ setup(
     name='guesstidate',
     version=get_version("guesstidate/__init__.py"),
     packages=['guesstidate'],
-    url='https://github.com/nedap/dateinfer',
+    url='https://github.com/Fraunhofer-FIT-DSAI-PM/guesstidate',
     license='Apache 2.0',
     author='Jeffrey Starr, Fraunhofer FIT',
     author_email='will@pedalwrencher.com, sebastiaan.van.zelst@fit.fraunhofer.de, alessandro.berti@fit.fraunhofer.de',
